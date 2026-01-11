b2d4251a204a93405d64317dc9e0d294f84656a3
amaan118921 <amaanurrahman00@gmail.com>
2026-01-11 19:54:13 +0530

    remmoved user id from rate limitter

--- src/main/java/com/example/notes_spring/interceptor/RateLimitInterceptor.java
+++ src/main/java/com/example/notes_spring/interceptor/RateLimitInterceptor.java
@@ -1,10 +1,9 @@
 package com.example.notes_spring.interceptor;
 
-import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.example.notes_spring.component.AuthUtil;
 import com.example.notes_spring.service.RateLimitService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,11 +13,10 @@ import jakarta.servlet.http.HttpServletResponse;
 public class RateLimitInterceptor implements HandlerInterceptor {
 
     private final RateLimitService rateLimitService;
-    private final AuthUtil authUtil;
 
-    public RateLimitInterceptor(RateLimitService rateLimitService, AuthUtil authUtil) {
+
+    public RateLimitInterceptor(RateLimitService rateLimitService) {
         this.rateLimitService = rateLimitService;
-        this.authUtil = authUtil;
     }
 
 
@@ -26,14 +24,7 @@ public class RateLimitInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
         
-          String key;
-          
-            if(SecurityContextHolder.getContext().getAuthentication() != null) {
-                key = "USER_" + authUtil.getCurrentUserId();
-          }
-          else {
-                key = request.getRemoteAddr();
-          }
+          String key = request.getRemoteAddr();
 
         if (!rateLimitService.isRequestAllowed(key)) {
             response.setStatus(429); // Too Many Requests
